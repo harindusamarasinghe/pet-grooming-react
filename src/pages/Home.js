95fd0bb746141f3d9d56f1a1d147eed6483a5329
HarinduTheCoder <samarasinghe.harindu@gmail.com>
2024-08-16 21:03:14 +0530

    fixing a code smell in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import MainSlider from '../components/MainSlider';
-import Find from '../components/Find'
 import Counter from '../components/Counter'
 import Appoinment from '../components/Adoption'
 import BreedServices from '../components/BreedServices'
 import Faq from '../components/Faq'
 import Brand from '../components/Brand'
 import AdoptionFaq from '../components/adoptions/AdoptionFaq';
-import AdoptionShop from '../components/AdoptionShop'
 import Testimonial from '../components/Testimonial'
 import Newsletter from '../components/NewsLetter'
-import BlogsHome from '../components/BlogsHome'
-import HomeTwoSlider from '../components/hometwo/HomeTwoSlider'
 
 
 
