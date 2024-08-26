49da5226d56eca41be205c0ef423104108570601
HarinduTheCoder <samarasinghe.harindu@gmail.com>
2024-08-26 21:02:06 +0530

    Removing un-used variables

--- src/components/adoptions/AdoptionSlider.js
+++ src/components/adoptions/AdoptionSlider.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import HomeCounter from '../hometwo/HomeCounter'
-import Slider from 'react-slick'
 
 function AdoptionSlider() {
   return (
--- src/pages/AdoptionsPages.js
+++ src/pages/AdoptionsPages.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import AdoptionBreadcumb from '../components/adoptions/AdoptionBreadcumb';
 import AdoptionSlider from '../components/adoptions/AdoptionSlider'
-import Appoinment from '../components/Adoption';
-import AdoptionPuppies from '../components/adoptions/AdoptionPuppies'
-import AdoptionFaq from '../components/adoptions/AdoptionFaq'
-import AdoptionGallery from '../components/adoptions/AdoptionGallery'
 
 function AdoptionsPages() {
 
